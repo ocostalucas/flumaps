ea87ba6e0a53584a3edb9cc04a27c624f5e1c6ea
ocostalucas <olucascosta98@gmail.com>
2020-08-07 15:42:49 -0300

    Initial commit

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_maps/app/screens/home/home_screen.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Maps',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
